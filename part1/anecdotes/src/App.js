77dfd6fecf7ce3b6031153cc0ba6340e4d9f8921
robinmartini <robin.martini@saltini.nl>
2021-10-27 21:41:27 +0200

    1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -12,10 +12,21 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length));
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button
+        onClick={() => {
+          const copy = [...votes];
+          copy[selected] += 1;
+          setVotes(copy);
+        }}
+      >
+        vote
+      </button>
       <button
         onClick={() =>
           setSelected(Math.floor(Math.random() * anecdotes.length))
